f81634d520492bee014823ec9db306b8e99553c2
secoro <deroosean@gmail.com>
2020-01-08 13:47:56 +0100

    search list bol.com and create a list of items and set each attribute accordingly

--- src/main/java/WebSpiderLauncher.java
+++ src/main/java/WebSpiderLauncher.java
@@ -1,6 +1,7 @@
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomAttr;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import model.Item;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.logging.Logger;
 public class WebSpiderLauncher {
 
     private static final Logger logger = Logger.getLogger(WebSpiderLauncher.class.getName());
-    public static final String baseUrl = "https://www.bol.com/nl/l/boeken/N/8299/?bltgh=s-iDlYBGQPgvgTt07k5PWA.2_3_4.5.CategoryImage";
+    public static final String baseUrl = "https://www.bol.com/nl/l/tablets-accessoires/N/18147/?promo=electronics_360__A_42602-42621-tablets-&-accessoires_8_";
     public static final String baseSecondUrl = "https://www.bol.com";
 
     public static void main(String[] args) {
@@ -19,26 +20,29 @@ public class WebSpiderLauncher {
         client.getOptions().setCssEnabled(false);
         client.getOptions().setUseInsecureSSL(true);
 
-        List<String> urls = new ArrayList<String>();
-
         try {
             HtmlPage page = client.getPage(baseUrl);
-            List<DomAttr> items = (List<DomAttr>) page.getByXPath("//*[@id='js_items_content']/li/div/div/div/a/@href");
-            if (items.isEmpty()) {
-                System.out.println("No items found");
-            } else {
-                for (DomAttr item: items) {
-                    urls.add(baseSecondUrl + item.getValue());
+            List<DomAttr> titles = (List<DomAttr>) page.getByXPath("//*/a[contains(@class, 'product-image product-image--list px_list_page_product_click')]/img/@alt");
+            List<DomAttr> prices = (List<DomAttr>) page.getByXPath("//*[@id='js_items_content']/li/div/wsp-buy-block/div/section/div/div/meta/@content");
+            List<DomAttr> urls = (List<DomAttr>) page.getByXPath("//*[@id='js_items_content']/li/div/div/a[contains(@class, 'hit-area__link medium--is-hidden')]/@href");
+
+
+            if (titles.size() == prices.size() && prices.size() == urls.size()) {
+                List<Item> items = new ArrayList<Item>(titles.size());
+                for (int i = 0; i < titles.size(); i++) {
+                    Item item = new Item();
+                    item.setTitle(titles.get(i).getTextContent());
+                    item.setPrice(Double.parseDouble(prices.get(i).getValue()));
+                    item.setUrl(baseSecondUrl + urls.get(i).getValue());
+                    items.add(item);
+                }
+
+                for (Item item: items) {
+                    System.out.println(item.toString());
                 }
             }
         } catch (IOException e) {
             logger.info(e.getMessage());
         }
-
-        System.out.println("***** URLS *****");
-        for (String item: urls) {
-            System.out.println(item);
-        }
-
     }
 }
